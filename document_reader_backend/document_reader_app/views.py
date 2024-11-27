f0a91d1cf2956592e8034b8a264645a01d8f109d
aprakash-spearhead <abhishek.prakash@gmail.com>
2024-11-27 15:35:18 +0530

    Django application set up + docker-compose

--- document_reader_backend/document_reader_app/views.py
+++ document_reader_backend/document_reader_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework import status
+from rest_framework.decorators import api_view
 
 # Create your views here.
+@api_view(['GET'])
+def hello_world(request):
+    
+    return Response('Hello World', status=status.HTTP_200_OK)
+
--- document_reader_backend/document_reader_backend/settings.py
+++ document_reader_backend/document_reader_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-jy3=4hr46i(2t+(h!$!l=s5yf$j^qiq+9s0aa1*4b31%4ezi%c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+    'document_reader_app',
 ]
 
 MIDDLEWARE = [
